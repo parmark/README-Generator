db090b5bfe7cc7e8000e951d9e3cf66abe59f7b8
Kevin Parmar <parmarkevinb@gmail.com>
2020-02-24 02:04:10 -0500

    filled questions constant and made inquirer and fs variables

---
+++ index.js
@@ -0,0 +1,21 @@
+var inquirer = require("inquirer");
+var fs = require("fs")
+
+const questions = [
+"Github username?",
+"Project title?",
+"Description?",
+"Table of Contents?",
+"Installation?", 
+"Usage?"
+];
+
+function writeToFile(fileName, data) {
+
+}
+
+function init() {
+    
+}
+
+init();
